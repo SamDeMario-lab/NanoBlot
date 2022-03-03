4b3ca56f02d1454893c83f20a9eb51123f9b0009
SamDeMario-lab <samdemario@chem.ucla.edu>
2022-03-03 11:31:11 -0800

    I started writing the main script

---
+++ IN_DEV.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+PLOTS="./plots_example.csv"
+META_DATA= "./data/Data_metadata.csv"
+
+while read plot_name loading_order probe_black probe_red probe_blue probe_yellow
+do
+  echo "---"
+  echo "Plot Name : $plot_name"
+  echo "Loading order : $loading_order"
+  echo "Probe : $probe_black"
+  echo "---"
+done < $PLOTS
+
+#read in data
+  #check formatting of data
+#read in probes
+  #check formatting of probes
+#read in blots
+  #check formatting of blots
+  #check if data is present for each blot
+  #check if probe is present for each blot
+#For loop through blots 
+  #Subset data based on loading order and probes save as temp data
+  #Make an array of file paths?
+#Pass file paths to an R script
+#generate plots
