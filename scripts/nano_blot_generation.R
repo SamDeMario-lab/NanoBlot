7adecef06997b0727777f4a23a3d2734f21c5572
SamDeMario-lab <samdemario@g.ucla.edu>
2022-05-18 13:10:01 -0700

    Made systematic generation of plot names

--- scripts/nano_blot_generation.R
+++ scripts/nano_blot_generation.R
@@ -2,11 +2,11 @@ library(ggplot2)
 library(Rsamtools)
 
 #Default Parameters
-duplication_factor <- 3
+duplication_factor <- 1
 
 #args = commandArgs(trailingOnly=TRUE)
 args <- "WT,RRP6,SLU7,RRP6SLU7"
-args[2] <- "RPS18A_Exon1"
+args[2] <- "RPL14A_Exon1"
 
 if (length(args)==0) {
   stop("This script requires 2 inputs. None detected.", call.=FALSE)
@@ -68,12 +68,28 @@ for (i in 1:duplication_factor) {
 blot_data$row_number_fuzz <- blot_data$row_number + runif(nrow(blot_data), min = -0.45, max = 0.45)
 
 #Plot graph
-ggplot(data = blot_data, aes(x = row_number_fuzz, y = qwidth))+
-  geom_point(alpha = 0.1, color = "black", size = 2)+
-  theme(axis.line = element_line(colour = "black"),
+plot_fuzzed <- ggplot(data = blot_data, aes(x = row_number_fuzz, y = qwidth))+
+  geom_point(alpha = 0.05, color = "black", size = 2)+
+  theme(axis.line = element_line(colour = "white"),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
         panel.border = element_blank(),
         panel.background = element_blank())+
   ylab(label = "Size in nts")+
   xlab(label = "")
+
+pre_plot_name <- paste("_" ,c(strsplit(args[1], split = ",")[[1]]), sep = "", collapse = "")
+
+
+plot_name <- paste("./plots/nanoblot__", args[2] ,"_",pre_plot_name, ".png", sep = "")
+
+ggsave(filename = plot_name ,plot = plot_fuzzed)
+# ggplot(data = blot_data, aes(x = row_number_fuzz, y = qwidth))+
+#   stat_density_2d(aes(fill = ..density..), geom = "raster", contour = FALSE)+
+#   theme(axis.line = element_line(colour = "white"),
+#         panel.grid.major = element_blank(),
+#         panel.grid.minor = element_blank(),
+#         panel.border = element_blank(),
+#         panel.background = element_blank())+
+#   ylab(label = "Size in nts")+
+#   xlab(label = "")
