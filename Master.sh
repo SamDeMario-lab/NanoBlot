e9accbec732f117c9027f98cb176aecc757fe316
Kevin Xu <kevinxu264@ucla.edu>
2022-08-21 15:30:58 -0700

    working on normalization

--- Master.sh
+++ Master.sh
@@ -162,94 +162,13 @@ then
 		fi
 		
 		BAMS=${fields[1]} # This then gets the 1st index, 2nd column of each row? which is the loading order?
-		NORM_FOLDER="./temp/"$(echo "$BAMS" | sed -e 's/,/_/g')"_NORM" #This basically creates a new folder called norm
-		# and then changes the comma in the loading order to an underscore, the s stands for substitute function, whereas
-		# the g stands for a global change, since s by itself only applies to the first match
-		if [ ! -d "$NORM_FOLDER" ] # checks if the norm folder exists and is a directory 
-		then
-			echo $NORM_FOLDER " not found" #prints that the normalization folder is not found
-			mkdir -p $NORM_FOLDER; #The manual says that this creates intermediate directories as required. If this option is not specified, 
-			# the full path prefix of each operant must already exist --> this allows it so that you can create sub directories of a directory
-			# It will create the parent directory first, and if it does exist, it will move to create further sub-directories 
-			NORM_METADATA_FILE=$NORM_FOLDER"/data_metadata.csv" #Creates the metadata file type in the norm folder 
-			NORM_METADATA_HEADER="Sample_name (This must be unique for each sample)  Type (FAST5 or BAM)  Location (For BAM inputs the path to the bam file should be given.)"
-			# Above code is the header 
-			echo "Starting Normalization"
-			echo "Samples: "$BAMS
-			IFS=',' #Changes the input field separator to commas now 
-			echo ${NORM_METADATA_HEADER} >$NORM_METADATA_FILE # This creates the first line to be inputted into the empty csv file, which 
-			# basically only contains the header first 
-			COMPARE=999999999999999 #This is probobly not the best way to do this, agreed, this is hard coding
-			for f in $BAMS; #Remember, the IFS changed to commas, so now this will create a for loop run for each of the loading orders
-			do
-				for (( o=2; o<=$END_META; o++ )) #Nested for loop, feel like there might be some easier way to do this as well 
-				do
-					DATA_LINE_o=$(head -n $o $META_DATA | tail -n -1) #Takes single rows again but this time out of the 
-					# metadata folder --> basically now it knows how to pull the BAM files 
-					IFS=$'\t'; read -a EELS <<<"$DATA_LINE_o" #Creates a new array called EELS --> this basically is each row in the metadata file
-					DATA_LOCATION=${EELS[1]} #Takes the 1st index, which is the value for the data location
-					SAMP_NAME=${EELS[0]} # Takes the 0th index, which is the value for the sample name
-					
-					#This if statement can just be calculated in one awk command
-					#awk -v var="$f" '$1==var {print $0}'
-					
-					if [[ $f == $SAMP_NAME ]] #If it finds the sample name, so like WT or RRP6 in the SAMP_NAME of the data metadata
-					then
-						echo "Found "$f 
-						READ_COUNT=$(samtools view -c -F 260 $DATA_LOCATION) # samtools view function will view and convert SAM/BAM/CRAM files
-						# the -c option counts the number of alignments instead of printing out the alignments 
-						# the -F flag does not output any alignments with the bits set in the Flag argument 
-						# a -F flag of 260 means that
-						# 256 - The given alignment is a secondary alignment 4 - The read was not aligned
-						# In essence, this makes it so that all unaligned and non-first choice mappings are removed
-						# In the future, we need a better way to store the sample files for reference 
-						if [[ $READ_COUNT -lt $COMPARE ]] # -lt is less than
-						then
-							COMPARE=$READ_COUNT # An easier way is to just declare the variable, I get that you want this variable to 
-							# exist outside the scope of the for loop 
-							# Will take the lowest read count if there are multiple read counts from multiple bam file locations for a single
-							# sample name 
-						fi
-					fi
-				done
-			done
-			echo $COMPARE # Prints out the lowest read count across all samples in one plot
-			IFS=',' #Changes the IFS again
-			
-			#After it finds all the metadata it needs to normalize the loading_order, it then normalizes the metadata sorted
-			# bam files to that normalized number 
-			for f in $BAMS;
-			do
-				for (( e=2; e<=$END_META; e++ ))
-				do
-					DATA_LINE=$(head -n $e $META_DATA | tail -n -1)
-					IFS=$'\t'; read -a EELS <<<"$DATA_LINE"
-					DATA_LOCATION=${EELS[1]}
-					SAMP_NAME=${EELS[0]}
-					if [[ $f == $SAMP_NAME ]]
-					then
-						echo "Subsetting "$SAMP_NAME" to "$COMPARE" reads."
-						OUTPUT_NORM_SAM=$NORM_FOLDER"/"$f"_NORM.sam"
-						OUTPUT_NORM_BAM=$NORM_FOLDER"/"$f"_NORM.bam"
-						cat <(samtools view -H $DATA_LOCATION) <(samtools view ${DATA_LOCATION} | shuf -n $COMPARE) > $OUTPUT_NORM_SAM #THIS TAKES A LOT OF RAM
-						# above line, -H means to output the header only, I think this is just doing header and body separately? 
-						# so the way this is normalizing is it just randomly takes the lowest number of reads across all sequenced files
-						# There has to be a better way to normalize using a package
-						samtools view -S -b $OUTPUT_NORM_SAM > $OUTPUT_NORM_BAM #-S, previously required if input was in SAM format
-						# now, it automatically detects -b outputs in bam format 
-						samtools sort $OUTPUT_NORM_BAM -o $OUTPUT_NORM_BAM #sorts, which sorts all the reads by genomic position
-						samtools index $OUTPUT_NORM_BAM #indexes the bam file, mainly for igv use, allows navigating it without
-						# loading all the file into memory
-						echo $SAMP_NAME"		"$OUTPUT_NORM_BAM >> $NORM_METADATA_FILE
-					fi
-				done
-			done
-		else #else loop runs if the norm directory already exists 
-			echo "" 
-			echo $NORM_FOLDER" already exists."
-			echo "If data normalization should be re-run delete "$NORM_FOLDER
-			echo ""
-		fi #finishes the if to check if the directory exists already for normalization
+		
+		# Psuedocode
+		# Creates count tables for each of the samples based off of their metadata location file
+		# Stores those count tables into a NORM folder
+		# This norm folder will then be later accessed in the nano_blot_generation which will call the normalization.R script 
+	
+	
 	done # finishes the first loop
 fi #finishes the first if to check if normalization is already done 
 
