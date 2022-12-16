a804e3d6c4e27fed650f270c61d5d5e637e1ee37
SamDeMario-lab <samdemario@chem.ucla.edu>
2022-12-16 11:20:20 -0800

    Initialized an R package. I wrote a number of funtions core to Nanoblot.
    There are 2 helper functions: isUnique() and extractNanoblotData() which are requied for lapply-ing a few things.
    
    scanBamFiles() is a command which 1) checks for uniqueness of SampleIDs and BamFiles 2) Runs Rsamtools::scanBam() and returns the output as a list of the outputs.
    
    Because of how this was implimented it creates a list with n objects of length 1. Each of these objects is a list of length 13 (the output of scanBams). This could be considered a bug but I'm intentially leaving it so that additional data can be added to the list without modifing the standard output of scanBams().
    
    bamFilesToNanoblotData() takes the output of  scanBamFiles() and returns a dataframe with the data required to make a NanoBlot. It uses the extractNanoblotData() helper function.
    
    Finally, makeNanoblot() takes the output of bamFilesToNanoblotData(), a metadata file, a type of blot and and optional plotTitle. It prints the Nanoblot.

---
+++ NanoBlotPackage/R/nanoBlot_package.R
@@ -0,0 +1,106 @@
+## acceptable plotType 'blot', 'violin', 'ridge', 'dataRDS'
+
+isUnique <-
+	function(vector) {
+		return(!any(duplicated(vector)))
+	}
+
+extractNanoblotData <-
+	function(SampleList) {
+		NBdf <- data.frame("qname"=SampleList[[1]][['qname']],
+							 "qwidth"=SampleList[[1]][['qwidth']])
+		return(NBdf)
+	}
+
+scanBamFiles <-
+	function(SampleID,
+					 BamFileLocations) {
+		print("Noice")
+## check that sampleIDs and locations are unique
+		if (!isUnique(SampleID)) {
+			stop("SampleID contains non-unique names. All IDs must be unique.")
+		}
+		if (!isUnique(BamFileLocations)) {
+			stop("BamFileLocations contains non-unique files. Check filenames.")
+		}
+		BamFileList <- lapply(BamFileLocations, Rsamtools::scanBam)
+		names(BamFileList) <- SampleID
+		return(BamFileList)
+	}
+
+bamFilesToNanoblotData <-
+	function(BamFileList) {
+		DataframesExtract <- lapply(BamFileList, extractNanoblotData)
+		nanoblotData <- do.call("rbind", DataframesExtract)
+		nanoblotData$'SampleID' <-
+			as.factor(vapply(strsplit(row.names(nanoblotData), "\\."), `[`, 1, FUN.VALUE =
+											 	character(1)))
+		return(nanoblotData)
+	}
+
+makeNanoblot <-
+	function(nanoblotData,
+					 plotInfo,
+					 blotType = 'blot',
+					 plotTitle = "") {
+## Start with the logical checks
+		infoCol = c("SampleID",
+								"SampleLanes", 
+								"SampleColors")
+		if (!identical(names(plotInfo),
+									infoCol)) {
+			stop("names(plotInfo) does not match expected values. Check formatting.")
+		}
+## Add check for if all of the SampleIDs exist in the nanoblotData		
+## Add sample lanes to nanoblotData
+		nanoblotData <- merge(nanoblotData, plotInfo, by="SampleID")
+## Add fuzz to lanes
+		columnWidth <- 0.25
+		nanoblotData$SampleLanesFuzz <-
+			nanoblotData$SampleLanes + runif(nrow(nanoblotData),
+																			 min = -columnWidth,
+																			 max = columnWidth)
+## Create Nanoblot
+		if (blotType == 'blot') {
+			NanoPlot <- ggplot2::ggplot(data = nanoblotData, ggplot2::aes(x = SampleLanesFuzz, y = qwidth))+
+				ggplot2::theme(axis.line = ggplot2::element_line(colour = "white"),
+							panel.grid.major = ggplot2::element_blank(),
+							panel.grid.minor = ggplot2::element_blank(),
+							panel.border = ggplot2::element_blank(),
+							panel.background = ggplot2::element_blank())+
+				ggplot2::scale_x_continuous(breaks = c(1:max(plotInfo$"SampleLanes")),
+			limits = c(
+				min(plotInfo$"SampleLanes") - columnWidth,
+				max(plotInfo$"SampleLanes") + columnWidth
+			))+
+				ggplot2::ylab(label = "Size in nts")+
+				ggplot2::xlab(label = "")
+			for (Sample in levels(nanoblotData$SampleID)) {
+				NanoPlot <- NanoPlot + ggplot2::geom_point(
+					data = nanoblotData[nanoblotData$SampleID == Sample,],
+					alpha = 0.01,
+					color = nanoblotData[nanoblotData$SampleID == Sample,]$SampleColors[[1]],
+					size = 1
+				)
+			}
+		}
+		
+		print("At least the function ran...")
+		print(NanoPlot)
+	}
+
+WT_RRP6Test <- data.frame(
+	SampleID = c("WT_snR4_ACT1","RRP6_snR4_ACT1"),
+	SampleLanes = c(1,2),
+	SampleColors = c('red','blue')
+)
+
+TestDataNames <- c("WT_snR4_ACT1", 
+									 "RRP6_snR4_ACT1")
+TestDataFiles <- c("C:/SMD/Nano_blot/data/example/WT_sorted_merged.bam",
+									 "C:/SMD/Nano_blot/data/example/RRP6_sorted_merged.bam")
+BamsWTRRP6 <- scanBamFiles(TestDataNames, TestDataFiles)
+
+NanoBlotDataWTRRP6 <- bamFilesToNanoblotData(BamsWTRRP6)
+
+makeNanoblot(nanoblotData = NanoBlotDataWTRRP6, plotInfo = WT_RRP6Test)
