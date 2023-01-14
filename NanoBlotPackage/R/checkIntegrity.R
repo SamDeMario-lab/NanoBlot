1382bddbc8d8fdde0d73d56cc9d5e33ca15487ef
SamDeMario-lab <samdemario@chem.ucla.edu>
2023-01-13 16:13:55 -0800

    Added a yml file which loads Bedtools (2.3.0), samtools (1.6), HTseq (2.0.2), R (3.5.1)
    Added a basically empty R file outlining checkIntegrity()

---
+++ NanoBlotPackage/R/checkIntegrity.R
@@ -0,0 +1,16 @@
+#' Calculate Integrity 
+#' 
+#' @param GeneTargets The result of running bamFilesToNanoblotData
+#' @param DataSet An optional title for the resulting plot.
+#' @export
+#' @examples
+#' makeNanoblot()
+#' 
+#' 
+
+calculateIntegrity <- function(GeneTargets, DataSet) {
+	print("This function should do somthing but it doesn't 
+I'm not sure what this is supposed to do. I needs to report what percentage of 
+transcripts are full length. As part of that the gene targets need to have known length
+and ideally be homgenious. This is way harder than it sounds. Review 3 can eat my ass. <3")
+}
